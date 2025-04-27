7d1a857ddfa733e8bfe9fcb382abf4dc3a62e44e
Gabriel <gabrieltux19@gmail.com>
2025-04-27 15:09:19 -0300

    feat: integrate front-end with back-end API, adjust endpoints, and update .sln file to combine both projects

--- GameStore.Api/Data/DataExtensions.cs
+++ GameStore.Api/Data/DataExtensions.cs
@@ -1,4 +1,4 @@
-using System;
+using GameStore.Api.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Api.Data;
@@ -9,6 +9,29 @@ public static class DataExtensions
     {
         using var scope = app.Services.CreateScope();
         var dbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
+
         await dbContext.Database.MigrateAsync();
+
+        if (!dbContext.Genres.Any())
+        {
+            dbContext.Genres.AddRange(
+                new Genre { Id = 1, Name = "Fighting" },
+                new Genre { Id = 2, Name = "Roleplaying" },
+                new Genre { Id = 3, Name = "Sports" },
+                new Genre { Id = 4, Name = "Racing" },
+                new Genre { Id = 5, Name = "Kids and Family" }
+            );
+            await dbContext.SaveChangesAsync();
+        }
+
+        if (!dbContext.Games.Any())
+        {
+            dbContext.Games.AddRange(
+                new Game { Id = 1, Name = "Street Fighter II", Price = 19.99M, GenreId = 1, ReleaseDate = new DateOnly(1922, 7, 15) },
+                new Game { Id = 2, Name = "Final Fantasy XIV", Price = 59.99M, GenreId = 2, ReleaseDate = new DateOnly(2010, 9, 30) },
+                new Game { Id = 3, Name = "FIFA 23", Price = 69.99M, GenreId = 3, ReleaseDate = new DateOnly(2022, 9, 27) }
+            );
+            await dbContext.SaveChangesAsync();
+        }
     }
 }
--- GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+693eacbe9d02590b7980007ae2fb5a1cd2eb63d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb499891d38cb5fea94aafbb6c2a765d36563505")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
